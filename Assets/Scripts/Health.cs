cc13abf88cabd9165435b0f86f1354fcac0a1007
dbowton <80086095+dbowton@users.noreply.github.com>
2023-03-29 15:28:09 -0600

    Health Script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+	[SerializeField] float maxHealth = 10;
+	[SerializeField] UnityEvent OnDeath;
+	private float currentHealth;
+
+	public float CurrentHealth { get { return currentHealth; } }
+
+	public void Damage(float value)
+	{
+		if (currentHealth > 0)
+		{
+			currentHealth -= value;
+
+			if (currentHealth <= 0)
+			{
+				OnDeath.Invoke();
+			}
+		}
+	}
+
+	public void Heal(float value)
+	{
+		currentHealth = Mathf.Min(currentHealth + value, maxHealth);
+	}
+}
