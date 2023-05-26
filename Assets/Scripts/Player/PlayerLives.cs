b6cb6c7bc810b3c8085a758ce6a0bcde3f9a7020
rileybazan115 <80065065+rileybazan115@users.noreply.github.com>
2023-05-26 16:07:03 -0600

    lvl 4

---
+++ Assets/Scripts/Player/PlayerLives.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLives : MonoBehaviour
+{
+	[SerializeField] GameObject spawner1;
+	[SerializeField] GameObject spawner2;
+	public int playerLives = 3;
+	public GameObject gameOverUI;
+
+    public void OnDeath()
+	{
+		playerLives--;
+		GetSpawner(spawner1, spawner2);
+		if (playerLives == 0)
+		{
+			gameOverUI.GetComponent<GameOver>().EndGame();
+			
+		}
+	}
+
+	public void GetSpawner(GameObject spawner1, GameObject spawner2)
+	{
+		if (Vector3.Distance(this.transform.position, spawner1.transform.position) > Vector3.Distance(this.transform.position, spawner2.transform.position))
+		{
+			spawner1.GetComponent<Respawn>().Trigger();
+		}
+		else
+		{
+			spawner2.GetComponent<Respawn>().Trigger();
+		}
+	}
+}
